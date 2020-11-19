7141f6e84c081a7ccda1d3351a9e7ec2a1fd6205
ste <stefano2890@gmail.com>
2020-11-19 14:59:19 +0100

    test

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include("days/days.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:title" content="Test prova prova" />
+    <meta property="og:image" itemprop="image" content="img/day/<?php echo $current_day ?>">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="day-box">
+        <a href="message.php"><img src="img/day/<?php echo $current_day ?>" alt=""></a>
+
+    </div>
+
+</div>
+    
+</body>
+</html>
