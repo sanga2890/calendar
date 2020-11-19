7d78681ba08040ada2cb5cec3fe2034c0a5a17fd
ste <stefano2890@gmail.com>
2020-11-19 18:10:54 +0100

    let's try again

--- days/days.js
+++ days/days.js
@@ -129,6 +129,9 @@ images[25] =
    
 let date =  new Date();
 let today = date.getDate();
+console.log(today);
+
+
 
 $.each(images, function( key, image ) {
 if (today == key) {
@@ -136,9 +139,12 @@ if (today == key) {
     let day_message = image.link;
     $('.current-day').attr('src' , "img/day/" + current_day);
     $('.current-message').attr('src' , "img/message/" + day_message);
-    $('head').prepend('<meta property="og:image" content="https://calendarioavventofamigliabrambilla.netlify.app/img/day/' + current_day + '"' + '>')
-   
+    // $('head').prepend('<meta property="og:image" content="https://calendarioavventofamigliabrambilla.netlify.app/img/day/' + current_day + '"' + '>')
+    return false
+} else {
+    console.log('current_day'); 
 }
+
 });
     
 
