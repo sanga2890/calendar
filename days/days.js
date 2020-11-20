31c74f67d0e8130f762a23578daec9e6910631fe
ste <stefano2890@gmail.com>
2020-11-21 00:09:59 +0100

    upload images

--- days/days.js
+++ days/days.js
@@ -16,127 +16,127 @@ $(document).ready(function(){
     let images = [];
     images[1] =  
     {
-        "day" : "1-01.png",
+        "day" : "1.png",
         "link" : "20.gif" 
     }
     images[2] = 
     {
-        "day" : "2-01.png",
+        "day" : "2.png",
         "link": "2"
     }
     images[3] = 
     {
-        "day" : "3-01.png",
+        "day" : "3.png",
         "link" :"3"
     }
     images[4] = 
     {
-        "day" : "4-01.png",
+        "day" : "4.png",
         "link" : "4"
     }
     images[5] = 
     {
-        "day" : "5-01.png",
+        "day" : "5.png",
         "link" : "5"
     }
     images[6] = 
     {
-        "day" : "6-01.png",
+        "day" : "6.png",
         "link" : "6"
     }  
     images[7] = 
     {
-        "day" : "7-01.png",
+        "day" : "7.png",
         "link" : "7"
     }
     images[8] = 
     {
-        "day" : "8-01.png",
+        "day" : "8.png",
         "link" : "8"
     }
     images[9] = 
     {
-        "day" : "9-01.png",
+        "day" : "9.png",
         "link" : "9"
     } 
     images[10] = 
     {
-        "day" : "10-01.png",
+        "day" : "10.png",
         "link" : "10"
     }  
     images[11] = 
     {
-        "day" : "11-01.png",
+        "day" : "11.png",
         "link" : "11"
     }
     images[12] = 
     {
-        "day" : "12-01.png",
+        "day" : "12.png",
         "link" : "12"
     }  
     images[13] = 
     {
-        "day" : "13-01.png",
+        "day" : "13.png",
         "link" : "13"
     }
     images[14] = 
     {
-        "day" : "14-01.png",
+        "day" : "14.png",
         "link" : "14"
     }
     images[15] = 
     {
-        "day" : "15-01.png",
+        "day" : "15.png",
         "link" : "15"
     }
     images[16] = 
     {
-        "day" : "16-01.png",
+        "day" : "16.png",
         "link" : "16"
     }  
     images[17] = 
     {
-        "day" : "17-01.png",
+        "day" : "17.png",
         "link" : "17"
     } 
     images[18] = 
     {
-        "day" : "18-01.png",
+        "day" : "18.png",
         "link" : "18"
     }  
     images[19] = 
     {
-        "day" : "19-01.png",
+        "day" : "19.png",
         "link" : "19.gif"
     } 
     images[20] = 
     {
-        "day" : "20-01.png",
+        "day" : "20.png",
         "link" : "20.gif"
     }   
     images[21] = 
     {
-        "day" : "21-01.png",
+        "day" : "21.png",
         "link" : "22"
     }
     images[22] = 
     {
-        "day" : "22-01.png",
+        "day" : "22.png",
         "link" : "22"
     }  
     images[23] = 
     {
-        "day" : "23-01.png",
+        "day" : "23.png",
         "link" : "23"
     }   
     images[24] = 
     {
-        "day" : "24-01.png",
+        "day" : "24.png",
         "link" : "24"
     }
     images[25] = 
     {
-        "day" : "25-01.png",
+        "day" : "25.png",
         "link" : "25"
     }
     
