b1a01ea198ee2e96b877a121389365f247f29154
ste <stefano2890@gmail.com>
2020-11-22 21:58:09 +0100

    fixed for real second time

--- days/days.js
+++ days/days.js
@@ -153,7 +153,7 @@ $(document).ready(function(){
                 let day_message = image.link;
                 $('.current-day').attr('src' , "img/day/" + current_day);
                 $('.current-message').attr('src' , "img/message/" + day_message);
-                $('head').prepend('<meta property="og:title" content="Calendario dell\'Avvento - Famiglia Brambilla - ' + today + ' ' + 'Dicembre' + '"'  + '/>')
+                $('head').prepend('<meta property="og:title" content=' + '"' + today + ' ' + 'Dicembre' + '"'  + '/>')
                 return false
             } 
             
